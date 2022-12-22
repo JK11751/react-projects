959717e61081636c5b96ad767b25107eeaef0e75
Gimo <kgimase@gmail.com>
2022-12-22 18:28:33 +0300

    initial commits

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,8 +1,27 @@
 import React, { useState } from 'react';
+//import {Box, Heading,Button }from "@chakra-ui/react"
 import data from './data';
 import List from './List';
-function App() {
-  return <h2>reminder project setup</h2>;
+const App=()=> {
+  const[people,setPeople]=useState(data)
+  return (
+<main>
+<section>
+<h3>{people.length} birthdays today</h3>
+<List people={people} />
+
+<button onClick={()=> setPeople([])} >clear all</button>
+
+
+</section>
+
+
+</main>
+
+
+
+
+  )
 }
 
 export default App;
